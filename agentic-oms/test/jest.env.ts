6967c1eee76724f37531f5ad3c79c3aa60a9ee58
deepakpalpro <deepak.pal.pro@gmail.com>
2025-12-28 16:13:07 +1100

    Add Jest environment variables for testing

---
+++ agentic-oms/test/jest.env.ts
@@ -0,0 +1,4 @@
+process.env.AWS_REGION = 'ap-southeast-2';
+process.env.ORDER_TABLE_NAME = 'OrderEventsTest';
+process.env.EVENT_BUS_NAME = 'OrderBusTest';
+process.env.METRICS_NAMESPACE = 'OMS/Test';
